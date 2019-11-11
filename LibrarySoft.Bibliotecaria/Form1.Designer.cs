3e147480457c13d1acce0450799186de45874c31
Carlos <carlos.vazquez.unach@gmail.com>
2019-11-10 18:45:13 -0600

    Initial commit

---
+++ LibrarySoft.Bibliotecaria/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace LibrarySoft.Bibliotecaria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtMatricula = new System.Windows.Forms.TextBox();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(56)))), ((int)(((byte)(107)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(88)))), ((int)(((byte)(152)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(13, 283);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(681, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Consultar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtMatricula
+            // 
+            this.txtMatricula.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMatricula.ForeColor = System.Drawing.Color.Silver;
+            this.txtMatricula.Location = new System.Drawing.Point(13, 230);
+            this.txtMatricula.MaxLength = 7;
+            this.txtMatricula.Name = "txtMatricula";
+            this.txtMatricula.Size = new System.Drawing.Size(681, 31);
+            this.txtMatricula.TabIndex = 4;
+            this.txtMatricula.Text = "Matrícula del alumno";
+            this.txtMatricula.Enter += new System.EventHandler(this.txtMatricula_Enter);
+            this.txtMatricula.Leave += new System.EventHandler(this.txtMatricula_Leave);
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LibrarySoft.Bibliotecaria.Properties.Resources.logoUnach;
+            this.pictureBox1.Location = new System.Drawing.Point(212, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(239, 195);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 449);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtMatricula);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LibrarySoft";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtMatricula;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+    }
+}
+
---
+++ LibrarySoft.Bibliotecaria/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LibrarySoft.Bibliotecaria
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void placeHolder(String texto, String texto2, Color color)
+        {
+            if (txtMatricula.Text == texto)
+            {
+                txtMatricula.Text = texto2;
+                txtMatricula.ForeColor = color;
+            }
+        }
+
+        private void txtMatricula_Leave(object sender, EventArgs e)
+        {
+            placeHolder("", "Matrícula del alumno", Color.Silver);
+        }
+
+        private void txtMatricula_Enter(object sender, EventArgs e)
+        {
+            placeHolder("Matrícula del alumno", "", Color.Black);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtMatricula.Text) || txtMatricula.Text == "Matrícula del alumno" || txtMatricula.Text.Length < 7)
+            {
+                MessageBox.Show("Ingresa una matrícula", "Campo obligatorio", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {                
+                if (txtMatricula.Text.ToLower() == "a150063")
+                {
+                    MessageBox.Show("El alumno actualmente tiene un adeudo en la biblioteca ", "Adeudo de libros", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    printPreviewDialog1.Document = printDocument1;
+                    printPreviewDialog1.ShowDialog();
+                }
+            }
+       
+        }
+        private void escribir(System.Drawing.Printing.PrintPageEventArgs e, String texto, Font font,  int x, int y)
+        {
+            e.Graphics.DrawString(texto, font, Brushes.Black, x, y);
+        }
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Bitmap imagen = Properties.Resources.unach_logo;
+            Image logo = imagen;
+            Font normal = new Font("Arial", 14, FontStyle.Regular);
+            Font italica = new Font("Arial", 14, FontStyle.Italic);
+            Font negrita = new Font("Arial", 14, FontStyle.Bold);
+
+            int inicio = 270;
+            int interlineado = 50;
+
+            e.Graphics.DrawImage(logo, 10, 10, logo.Width/4, logo.Height/4);
+            escribir(e, "Constancia de no adeudo de libros", negrita, 270, 10);
+            escribir(e, "Fecha: " + DateTime.Today.ToShortDateString(), negrita, 660, 60);
+
+            escribir(e, "La presente hace constar que el alumno ",normal, 130, inicio);
+            escribir(e, "Carlos Alberto Vazquez Farrera", negrita, 475, inicio);
+            escribir(e, ",", normal, 760, inicio);
+
+            escribir(e, "con Matrícula", normal, 40, inicio + interlineado);
+            escribir(e, "A150064", negrita, 160, inicio + interlineado);
+            escribir(e, ", actualmente no tiene ningún adeudo en la biblioteca de esta área", normal, 235, inicio + interlineado);
+
+            escribir(e, "y plantel educativo.", normal, 40, (inicio + interlineado * 2));
+
+            escribir(e, "Se extiende la presente constancia a petición del interesado(a) y para los", normal, 130, (inicio + interlineado * 5));
+            escribir(e, "fines que a este convenga.", normal, 40, (inicio + interlineado * 6));
+
+
+            escribir(e, "ATENTAMENTE", negrita, 345, (inicio + interlineado * 10));
+            escribir(e, "\"POR LA CONCIENCIA DE LA NECESISDAD DE SERVIR\"", negrita, 160, (inicio + interlineado * 11));
+
+
+
+
+            escribir(e, "Biblioteca de la Licenciatura en Sistemas Computacionales", italica, 330, 1075);
+
+        }
+    }
+}
